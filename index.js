1809b796ec78f0d3305358752bc5132d8f5f1962
unknown <180530@bentoquirino.edu>
2020-03-02 14:17:04 -0300

    iniciando com Node

---
+++ index.js
@@ -0,0 +1,6 @@
+const http= require('http');
+http.createServer((req,res)=>{
+    res.writeHead(200, {'Content-Type':'text/html;charset-utf-8'});
+    res.end('Hello Word!');
+}).listen(3000);
+console.log('O node esta rodando na porta 3000, para encerrar o serviço precione Ctrol+C');
